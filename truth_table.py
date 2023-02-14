1c20f8f5de5e959fb77135f08de50daa3f693aa0
Gerald Aguilar <geraldag@usc.edu>
2023-02-13 19:46:32 -0800

    added condition checking to the __init__ method. Errors out if conditions are not unique

--- truth_table.py
+++ truth_table.py
@@ -6,9 +6,31 @@ class TruthTable(object):
 
     def __init__(self, name, conditions):
         self._name = name
-        self._conditions = conditions
+        self._conditions = self._validate_conditions(conditions)
         self._num_conditions = len(self._conditions) - 1
         self._n2 = self._n2_truth_table()
+        self._row = namedtuple(self._name, self._conditions)
+        self._table = self._create_table()
+
+    def __iter__(self):
+        for row in self._table:
+            yield row
+    
+    def _validate_conditions(self, conditions):
+        if len(conditions) == len(set(conditions)):
+            return conditions
+        else:
+            raise ValueError('Condition labels are not unique')
+
+    def _create_row(self, table_row):
+        return self._row(*table_row)
+
+    def _create_table(self):
+        table = []
+        for row in self._n2:
+            table.append(self._create_row(row))
+        
+        return table
 
     def _n2_truth_table(self):
         truth_table = itertools.product(self.STATES, 
@@ -34,8 +56,9 @@ class TruthTable(object):
         return [self._convert_to_bools(row) for row in truth_table if sum(row) in n2_conditions]
 
 if __name__ == '__main__':
-    conditions = ['c0', 'c1', 'result']
+    conditions = ['c0', 'c1', 'c2', 'c3', 'result']
 
     test1 = TruthTable("Test1", conditions)
 
-    print(test1._n2)
+    for row in test1:
+        print(row)
