80757f86f19931ee4fc0c67990fdc3de6914bfb1
Gerald Aguilar <geraldag@usc.edu>
2023-02-13 19:33:54 -0800

    generate n+2 truth table values

--- truth_table.py
+++ truth_table.py
@@ -1,4 +1,41 @@
 import itertools
+from collections import namedtuple
 
 class TruthTable(object):
-    pass
+    STATES = [0, 1]
+
+    def __init__(self, name, conditions):
+        self._name = name
+        self._conditions = conditions
+        self._num_conditions = len(self._conditions) - 1
+        self._n2 = self._n2_truth_table()
+
+    def _n2_truth_table(self):
+        truth_table = itertools.product(self.STATES, 
+                                             repeat=self._num_conditions)
+        n2_truth_table = self._n2_filter(truth_table)
+
+        return self._results(n2_truth_table)
+
+
+    def _results(self, truth_table):
+        for i, row in enumerate(truth_table):
+            result = all(row)  # TODO: add custom rule handling
+            row_with_results = list(row) + [result]
+            truth_table[i] = tuple(row_with_results)
+        
+        return truth_table
+
+    def _convert_to_bools(self, row):
+        return tuple([bool(x) for x in row])
+
+    def _n2_filter(self, truth_table):
+        n2_conditions = [0, self._num_conditions-1, self._num_conditions]
+        return [self._convert_to_bools(row) for row in truth_table if sum(row) in n2_conditions]
+
+if __name__ == '__main__':
+    conditions = ['c0', 'c1', 'result']
+
+    test1 = TruthTable("Test1", conditions)
+
+    print(test1._n2)
